c2b083879a9c80a4bf73926516d5cf2597a2dabf
kanghsienchang <kanghsien@owlting.com>
2022-06-14 10:42:14 +0800

    update

--- plugins/gtm.js
+++ plugins/gtm.js
@@ -4,7 +4,7 @@ import VueGtm from '@gtm-support/vue2-gtm'
 export default ({ app }) => {
   Vue.use(VueGtm, {
     id: process.env.NUXT_GTM_ID,
-    enabled: process.env.NODE_ENV === 'development',
+    enabled: process.env.NUXT_GTM_ENABLED === 'true',
     vueRouter: app.router,
     trackOnNextTick: false
   })
